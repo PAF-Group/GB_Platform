71dba5de6e2ed6abaabdfc5c7f8751c99535ae91
Yasiru Randika <43146170+YasiruRandika@users.noreply.github.com>
2021-04-12 10:22:32 +0530

    Update OrderManagement.sql

--- Database/Table Creation/OrderManagement.sql
+++ Database/Table Creation/OrderManagement.sql
@@ -1,1 +1,87 @@
 
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 12, 2021 at 06:48 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gb_ordermanagement`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderdetails`
+--
+
+CREATE TABLE `orderdetails` (
+  `OrderId` int(11) NOT NULL,
+  `ProductId` int(11) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `UnitPrice` double NOT NULL,
+  `Status` varchar(20) NOT NULL DEFAULT 'Processing',
+  `ShippingDate` date DEFAULT NULL,
+  `ShippingCompany` varchar(100) DEFAULT NULL,
+  `ShipingTrackId` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `OrderId` int(11) NOT NULL,
+  `BuyerId` int(11) NOT NULL,
+  `Date` date NOT NULL DEFAULT current_timestamp(),
+  `Status` varchar(20) NOT NULL DEFAULT 'Not Paid',
+  `PaymentSlipUrl` varchar(200) DEFAULT NULL,
+  `paymentAccepted` tinyint(1) DEFAULT NULL,
+  `ShippingAddress` varchar(200) NOT NULL,
+  `TotalAmount` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orderdetails`
+--
+ALTER TABLE `orderdetails`
+  ADD PRIMARY KEY (`ProductId`,`OrderId`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`OrderId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `OrderId` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
