d39b1be5daca20669a552d092df6569554e95cdc
Yasiru Randika <43146170+YasiruRandika@users.noreply.github.com>
2021-04-12 23:59:24 +0530

    Add read all orders function

--- OrderManagement/src/com/OrderService.java
+++ OrderManagement/src/com/OrderService.java
@@ -9,10 +9,22 @@ import model.Orders;
 //For REST Service
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
 //For JSON
 import com.google.gson.*;
 
 @Path("/Orders")
 public class OrderService {
+	Orders orderModel = new Orders();
 	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String getItems() {
+		System.out.println("working");
+		//return Response.status(200).entity(orderModel.getAllOrders()).build();
+		return orderModel.getAllOrders();
+	}
 }
--- OrderManagement/src/model/Orders.java
+++ OrderManagement/src/model/Orders.java
@@ -24,4 +24,56 @@ public class Orders {
 		}
 		return con;
 	}
+	
+	/*
+	 * Read all records in the orders table
+	 * 
+	 * */
+	public String getAllOrders() {
+		String output;
+		try {
+			Connection con = connect();
+			if (con == null) {
+				output =  "Error while connecting to the database for reading.";
+				return output;
+			}
+			
+			// Prepare the html table to be displayed
+			output = "<table border='1'><tr><th>Order Id</th><th>Buyer Id</th>" + "<th>Date</th>"
+								+ "<th>Status</th>" + "<th>Total Amount</th>";
+			
+			//SQL Query for selecting all orders
+			String query = "select * from orders";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				
+				String orderId = Integer.toString(rs.getInt("OrderId"));
+				String buyerId  = Integer.toString(rs.getInt("BuyerId"));
+				String date  = rs.getDate("Date").toString();
+				String status = rs.getString("Status");
+				String totalAmount = Double.toString(rs.getDouble("TotalAmount"));
+				
+				// Add into the html table
+				output += "<tr><td>" + orderId + "</td>";
+				output += "<td>" + buyerId + "</td>";
+				output += "<td>" + date + "</td>";
+				output += "<td>" + status + "</td>";
+				output += "<td>" + totalAmount + "</td></tr>";
+				}
+			con.close();
+			// Complete the html table
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading the records.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
+	/*
+	 * Add order to the database
+	 * 
+	 * */
 }
