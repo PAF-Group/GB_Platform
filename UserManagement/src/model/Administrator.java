7cd40fc52b31751347ffe48f6b0eb64a37b33dee
Vishwa-Gunathilake <66120464+Vishwa-Gunathilake@users.noreply.github.com>
2021-04-21 13:03:42 +0530

    Updating Administrator.java file

--- UserManagement/src/model/Administrator.java
+++ UserManagement/src/model/Administrator.java
@@ -6,58 +6,49 @@
 package model;
 
 import java.sql.*;
+import com.DatabaseConnectivity;
 
 public class Administrator {
-//	Database/UserDB Connectivity; @return Connection ------------------------------------------------------------------------------------------------------------
-	private Connection connect() {
-		Connection con = null;
-		
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-
-			// Provide the UserDB details: DBServer/DBName, user-name, password
-			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/userdb", "root", "qwerty");
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			
-		}
-		
-		return con;
-		
-	}
-//	-------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 //	The method to create a new Administrator => Administrator registration --------------------------------------------------------------------------------------
-	public String createAdministrator(String empID, String firstName, String lastName, String userPhone, String email, String password, String accStatus) {
+	public String createAdministrator(String empID, String firstName, String lastName, String userPhone, String email, String password, String role, String accStatus) {
 		String output = "";
 		
 		try {
-			Connection con = connect();
+			Connection con = DatabaseConnectivity.connect();
 			
 			if (con == null) {
 				return "An error has occurred while connecting to the database.";
 				
 			}
 			
-			// The query to insert a new record to the Administrator table & prepared statements
-			String query = "INSERT INTO `userdb`.`administrator` (`employee_id`, `first_name`, `last_name`, `user_phone`,"
-					+ " `user_email`, `password`, `account_status`)" + " VALUES (?, ?, ?, ?, ?, ?, ?)";
+			// The query to insert a new record to the User table & prepared statements
+			String query1 = "INSERT INTO `userdb` .`user` (`user_email`, `password`, `user_role`, `accStatus`)" + " VALUES (?, ?, ?, ?)";
 			
-			PreparedStatement preparedStmt = con.prepareStatement(query);
+			PreparedStatement preparedStmt1 = con.prepareStatement(query1);
+			
+			// binding values
+			preparedStmt1.setString(5, email);
+			preparedStmt1.setString(6, password);
+			preparedStmt1.setString(7, role);
+			preparedStmt1.setString(8, accStatus);
+			
+			// execute the statement
+			preparedStmt1.execute();
+			
+			// The query to insert a new record to the Administrator table & prepared statements
+			String query2 = "INSERT INTO `userdb`.`administrator` (`employee_id`, `first_name`, `last_name`, `user_phone`)" + " VALUES (?, ?, ?, ?)";
+			
+			PreparedStatement preparedStmt2 = con.prepareStatement(query2);
 			
 			// binding values
 			//preparedStmt.setInt(1, 0);
-			preparedStmt.setInt(1, Integer.parseInt(empID));
-			preparedStmt.setString(2, firstName);
-			preparedStmt.setString(3, lastName);
-			preparedStmt.setString(4, userPhone);
-			preparedStmt.setString(5, email);
-			preparedStmt.setString(6, password);
-			preparedStmt.setString(7, accStatus);
+			preparedStmt2.setInt(1, Integer.parseInt(empID));
+			preparedStmt2.setString(2, firstName);
+			preparedStmt2.setString(3, lastName);
+			preparedStmt2.setString(4, userPhone);
 			
 			// execute the statement
-			preparedStmt.execute();
+			preparedStmt2.execute();
 			
 			// Close the database connection
 			con.close();
@@ -82,7 +73,7 @@ public class Administrator {
 		String output = "";
 		
 		try {
-			Connection con = connect();
+			Connection con = DatabaseConnectivity.connect();
 			
 			if (con == null) {
 				return "An error has occurred while connecting to the database.";
@@ -94,22 +85,29 @@ public class Administrator {
 					+  "<th>Email</th>" + "<th>Account Status</th>" + "<th>Created At</th>" + "<th>Updated At</th>" + "</tr>";
 
 			// The query to select all records from Administrator table
-			String query = "SELECT `employee_id`, `first_name`, `last_name`, `user_phone`, `user_email`, `account_status`, `created_at`, `updated_at` FROM `userdb`.`administrator`";
+			String query1 = "SELECT `employee_id`, `first_name`, `last_name`, `user_phone`, `user_id`, `created_at`, `updated_at` FROM `userdb`.`administrator`";
 			
-			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery(query);
+			Statement stmt1 = con.createStatement();
+			ResultSet set1 = stmt1.executeQuery(query1);
 			
 			// Iterate through all the records in the result set
-			while (rs.next()) {
-				String empID = Integer.toString(rs.getInt("employee_id"));
-				String firstName = rs.getString("first_name");
-				String lastName = rs.getString("last_name");
-				String phone = rs.getString("user_phone");
-				String email = rs.getString("user_email");
-				//String password = rs.getString("password");
-				String accStatus = rs.getString("account_status");
-				String createdAt = rs.getTimestamp("created_at").toString();
-				String updatedAt = rs.getTimestamp("updated_at").toString();
+			while (set1.next()) {
+				String empID = Integer.toString(set1.getInt("employee_id"));
+				String firstName = set1.getString("first_name");
+				String lastName = set1.getString("last_name");
+				String phone = set1.getString("user_phone");
+				String userID = set1.getString("user_id");
+				String createdAt = set1.getTimestamp("created_at").toString();
+				String updatedAt = set1.getTimestamp("updated_at").toString();
+				
+				// The query to select the certain Administrator record from the User table
+				String query2 = "SELECT `user_email`, `accStatus` FROM `userdb`.`user` WHERE `user_id` = " + userID;
+				
+				Statement stmt2 = con.createStatement();
+				ResultSet set2 = stmt2.executeQuery(query2);
+				
+				String email = set2.getString("user_email");
+				String accStatus = set2.getString("accStatus");
 				
 				// Add the record in to the HTML table
 				output += "<tr><td>" + empID + "</td>";
@@ -143,11 +141,11 @@ public class Administrator {
 //	-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //	The method to update an Administrator => a service for Administrator ----------------------------------------------------------------------------------------
-	public String updateAdministrator(String userID, String empID, String firstName, String lastName, String userPhone, String email) {
+	public String updateAdministrator(String adminID, String empID, String firstName, String lastName, String userPhone) {
 		String output = "";
 		
 		try {
-			Connection con = connect();
+			Connection con = DatabaseConnectivity.connect();
 			
 			if (con == null) {
 				return "An error has occurred while connecting to the database for updating.";
@@ -155,7 +153,7 @@ public class Administrator {
 			}
 			
 			// The query to Update the certain record in the Administrator table & prepared statements
-			String query = "UPDATE `userdb`.`administrator` SET `employee_id`=?, `first_name`=?, `last_name`=?, `user_phone`=?, `user_email`=? WHERE `user_id`=?";
+			String query = "UPDATE `userdb`.`administrator` SET `employee_id`=?, `first_name`=?, `last_name`=?, `user_phone`=? WHERE `admin_id`=?";
 			
 			PreparedStatement preparedStmt = con.prepareStatement(query);
 			
@@ -164,8 +162,7 @@ public class Administrator {
 			preparedStmt.setString(3, firstName);
 			preparedStmt.setString(4, lastName);
 			preparedStmt.setString(5, userPhone);
-			preparedStmt.setString(6, email);
-			preparedStmt.setInt(1, Integer.parseInt(userID));
+			preparedStmt.setInt(1, Integer.parseInt(adminID));
 			
 			// execute the statement
 			preparedStmt.execute();
@@ -189,26 +186,77 @@ public class Administrator {
 	
 //	-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//	The method to disable an existing Administrator by using user ID => for Administrator -------------------------------------------------------------------------------
-	public String disableAdministrator(String userID, String accStatus) {
+//	The method to update an Administrator's User-email & Password => a service for Administrator ----------------------------------------------------------------
+	public String updateEmailPassword(String userID, String userEmail, String password) {
 		String output = "";
 		
 		try {
-			Connection con = connect();
+			Connection con = DatabaseConnectivity.connect();
+			
+			if (con == null) {
+				return "An error has occurred while connecting to the database for updating.";
+				
+			}
+			
+			// The query to Update the certain record in the User table & prepared statements
+			String query = "UPDATE `userdb`.`user` SET `user_email`=?, `password`=? WHERE `user_id`=?";
+			
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			// binding values
+			preparedStmt.setString(2, userEmail);
+			preparedStmt.setString(3, password);
+			preparedStmt.setInt(1, Integer.parseInt(userID));
+			
+			// execute the statement
+			preparedStmt.execute();
+			
+			// Close the database connection
+			con.close();
+			
+			// Success
+			output = "Administrator User-email & Password records was Updated successfully!...";
+			
+		} catch (Exception e) {
+			// Failure
+			output = "An error has occurred while updating the Administrator record.";
+			System.err.println(e.getMessage());
+			
+		}
+		
+		return output;
+		
+	}
+	
+//	-------------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+//	The method to disable an existing Administrator by using user ID => for Administrator -----------------------------------------------------------------------
+	public String disableAdministrator(String adminID, String accStatus) {
+		String output = "";
+		
+		try {
+			Connection con = DatabaseConnectivity.connect();
 			
 			if (con == null) {
 				return "An error has occurred while connecting to the database for disabling.";
 				
 			}
 			
-			// The query to disable an Administrator & prepared statements
-			String query = "UPDATE `userdb`.`administrator` SET `account_status`=? WHERE `user_id`=?";
+			// The query to retrieve user ID from the Administrator table
+			String query1 = "SELECT `user_id` FROM `userdb`.`administrator` WHERE `admin_id` = " + adminID;
 			
-			PreparedStatement preparedStmt = con.prepareStatement(query);
+			Statement stmt = con.createStatement();
+			ResultSet set = stmt.executeQuery(query1);
+			
+			String userID = set.getString("user_id");
+			
+			// The query to disable an Administrator & prepared statements
+			String query2 = "UPDATE `userdb`.`user` SET `account_status`=? WHERE `user_id`=" + userID;
+			
+			PreparedStatement preparedStmt = con.prepareStatement(query2);
 			
 			// binding values
 			preparedStmt.setString(2, accStatus);
-			preparedStmt.setInt(1, Integer.parseInt(userID));
 			
 			// execute the statement
 			preparedStmt.execute();
